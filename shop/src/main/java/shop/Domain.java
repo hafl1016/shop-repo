deb18e18e306cc53f0cde7429e984a05008df095
Florian Haamann <hafl1016(t)hs-karlsruhe.de>
2013-10-29 14:03:48 +0100

    variablen ID, Vorname, Nachname mit gettern und settern erstellt

---
+++ shop/src/main/java/shop/Domain.java
@@ -0,0 +1,27 @@
+package shop;
+
+public class Domain {
+private String vorname;
+private String nachname;
+private int id;
+public String getVorname() {
+	return vorname;
+}
+public void setVorname(String vorname) {
+	this.vorname = vorname;
+}
+public String getNachname() {
+	return nachname;
+}
+public void setNachname(String nachname) {
+	this.nachname = nachname;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+
+
+}
