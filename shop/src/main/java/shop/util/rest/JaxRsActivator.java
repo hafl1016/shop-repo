a8180896f5d2ad69fd8144d9754b3a08f1d6eb4b
Florian Haamann <hafl1016(t)hs-karlsruhe.de>
2013-10-30 15:43:08 +0100

    Util hochgeladen

---
+++ shop/src/main/java/shop/util/rest/JaxRsActivator.java
@@ -0,0 +1,11 @@
+package shop.util.rest;
+
+import static shop.util.Constants.REST_PATH;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath(REST_PATH)
+public class JaxRsActivator extends Application {
+}
--- shop/src/main/java/shop/util/rest/UriHelper.java
+++ shop/src/main/java/shop/util/rest/UriHelper.java
@@ -1,5 +1,23 @@
+
 package shop.util.rest;
 
-public class UriHelper {
+import java.net.URI;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.UriInfo;
+
+@ApplicationScoped
+public class UriHelper {
+	public URI getUri(Class<?> clazz, UriInfo uriInfo) {
+		return uriInfo.getBaseUriBuilder()
+		              .path(clazz)
+		              .build();
+	}
+
+	public URI getUri(Class<?> clazz, String methodName, Long id, UriInfo uriInfo) {
+		return uriInfo.getBaseUriBuilder()
+		              .path(clazz)
+		              .path(clazz, methodName)
+		              .build(id);
+	}
 }
