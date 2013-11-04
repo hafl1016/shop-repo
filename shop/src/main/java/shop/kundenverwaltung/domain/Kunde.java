2e9c59e5e0418c3f621fb417a8b95082d1d60f35
Marius Faßbender <marius.fassbender@web.de>
2013-11-04 18:14:57 +0100

    Kunde.java überarbeitet

--- shop/src/main/java/shop/kundenverwaltung/domain/Kunde.java
+++ shop/src/main/java/shop/kundenverwaltung/domain/Kunde.java
@@ -2,16 +2,47 @@ package shop.kundenverwaltung.domain;
 
 import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlTransient;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 import shop.bestellverwaltung.domain.*;
 
 public class Kunde {
 
+	@NotEmpty
 	private Long id;
+	
+	@NotNull
+	@Size(min = 2, max = 32)
+	@Pattern(regexp = "[A-ZÄÖÜ][a-zäöüß]+")	//TODO Umlaute
 	private String nachname;
+	
+	@NotNull
+	@Size(min = 2,max = 32)
+	@Pattern(regexp = "[A-ZÄÖÜ][a-zäöüß]+") //TODO Umlaute
+	private String vorname;
+	
+	@NotNull
+	@Size(max = 128)
+	@Pattern(regexp = "[\\w.%-]+@[\\w.%-]+\\.[A-Za-z]{2,4}")
 	private String email;
+	
+	@NotNull
+	private String geburtsdatum;	//TODO Datefunktion nachschauen
+	
+	@NotNull
+	@Valid
 	private Adresse adresse;
 	
+	@NotNull
+	@Size(min = 1)
+	@Valid
 	@XmlTransient
 	private List<Bestellung> bestellungen;
 	
@@ -34,6 +65,18 @@ public class Kunde {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getGeburtsdatum() {
+		return geburtsdatum;
+	}
+	public void setGeburtsdatum(String geburtsdatum) {
+		this.geburtsdatum = geburtsdatum;
 	}
 	public Adresse getAdresse() {
 		return adresse;
@@ -58,7 +101,18 @@ public class Kunde {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result
+				+ ((bestellungen == null) ? 0 : bestellungen.hashCode());
+		result = prime * result
+				+ ((bestellungenUri == null) ? 0 : bestellungenUri.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((geburtsdatum == null) ? 0 : geburtsdatum.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((nachname == null) ? 0 : nachname.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
 		return result;
 	}
 	
@@ -71,19 +125,55 @@ public class Kunde {
 		if (getClass() != obj.getClass())
 			return false;
 		Kunde other = (Kunde) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (bestellungen == null) {
+			if (other.bestellungen != null)
+				return false;
+		} else if (!bestellungen.equals(other.bestellungen))
+			return false;
+		if (bestellungenUri == null) {
+			if (other.bestellungenUri != null)
+				return false;
+		} else if (!bestellungenUri.equals(other.bestellungenUri))
+			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
-		}
-		else if (!email.equals(other.email))
+		} else if (!email.equals(other.email))
+			return false;
+		if (geburtsdatum == null) {
+			if (other.geburtsdatum != null)
+				return false;
+		} else if (!geburtsdatum.equals(other.geburtsdatum))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nachname == null) {
+			if (other.nachname != null)
+				return false;
+		} else if (!nachname.equals(other.nachname))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
 			return false;
 		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "Kunde [id=" + id + ", nachname=" + nachname + ", email=" + email
-			   + ", bestellungenUri=" + bestellungenUri + "]";
+		return "Kunde [id=" + id + ", nachname=" + nachname + ", vorname="
+				+ vorname + ", email=" + email + ", geburtsdatum="
+				+ geburtsdatum + ", adresse=" + adresse + ", bestellungen="
+				+ bestellungen + ", bestellungenUri=" + bestellungenUri + "]";
 	}
 }
 
