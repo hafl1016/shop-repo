12833cd342032fe2da3c66c1315ad64899b2668e
Marius Faßbender <marius.fassbender@web.de>
2013-11-04 16:31:04 +0100

    class Adresse überarbeitet

--- shop/src/main/java/shop/kundenverwaltung/domain/Adresse.java
+++ shop/src/main/java/shop/kundenverwaltung/domain/Adresse.java
@@ -1,9 +1,29 @@
 package shop.kundenverwaltung.domain;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class Adresse {
 
-	private Long id;
+	@NotEmpty
+	private Long id;	//FIXME ist das noetig in der klasse adresse?
+	
+	@NotNull
+	@Size(min = 2, max = 32)
+	private String strasse;
+	
+	private String hausnummer;
+	
+	@NotNull
+	@Pattern(regexp = "\\d{5}")
 	private String plz;
+	
+	@NotNull
+	@Size(min = 2, max = 32)
+	@Pattern(regexp = "[A-ZÄÖÜ][a-zäöü]+")	//TODO Umlaute richtig deklarieren
 	private String ort;
 	
 	
@@ -14,6 +34,18 @@ public class Adresse {
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public String getHausnummer() {
+		return hausnummer;
+	}
+	public void setHausnummer(String hausnummer) {
+		this.hausnummer = hausnummer;
+	}
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
 	}
 	public String getPlz() {
 		return plz;
@@ -38,9 +70,13 @@ public class Adresse {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((hausnummer == null) ? 0 : hausnummer.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((kunde == null) ? 0 : kunde.hashCode());
 		result = prime * result + ((ort == null) ? 0 : ort.hashCode());
 		result = prime * result + ((plz == null) ? 0 : plz.hashCode());
+		result = prime * result + ((strasse == null) ? 0 : strasse.hashCode());
 		return result;
 	}
 	@Override
@@ -52,29 +88,43 @@ public class Adresse {
 		if (getClass() != obj.getClass())
 			return false;
 		Adresse other = (Adresse) obj;
+		if (hausnummer == null) {
+			if (other.hausnummer != null)
+				return false;
+		} else if (!hausnummer.equals(other.hausnummer))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		}
-		else if (!id.equals(other.id))
+		} else if (!id.equals(other.id))
+			return false;
+		if (kunde == null) {
+			if (other.kunde != null)
+				return false;
+		} else if (!kunde.equals(other.kunde))
 			return false;
 		if (ort == null) {
 			if (other.ort != null)
 				return false;
-		}
-		else if (!ort.equals(other.ort))
+		} else if (!ort.equals(other.ort))
 			return false;
 		if (plz == null) {
 			if (other.plz != null)
 				return false;
-		}
-		else if (!plz.equals(other.plz))
+		} else if (!plz.equals(other.plz))
+			return false;
+		if (strasse == null) {
+			if (other.strasse != null)
+				return false;
+		} else if (!strasse.equals(other.strasse))
 			return false;
 		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "Adresse [id=" + id + ", plz=" + plz + ", ort=" + ort + "]";
+		return "Adresse [id=" + id + ", strasse=" + strasse + ", hausnummer="
+				+ hausnummer + ", plz=" + plz + ", ort=" + ort + ", kunde="
+				+ kunde + "]";
 	}
 }
